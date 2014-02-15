8b4edce70a19a1142b77a97f2ee6c7e1dd9676e6
mingqin1 <mingqin@live.com>
2014-02-15 09:36:59 -0800

    updating

---
+++ src/main/java/com/example/RunThreadDeadLock.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+/**
+ *
+ * @author Ming Qin ( mingqin@live.com)
+ */
+public class RunThreadDeadLock {
+    
+    private static class LeftRightDeadLock implements Runnable{
+        private static final Object left = new Object();
+        private static final Object right = new Object();
+        private int number =1;  // change value of numbe to 4000,  to find 
+                                // thread starvation issue.
+        
+        public void doSomething( int number){
+            for ( int i=0; i< number; i++){
+                            char[] tmp = new char [1024*1024];
+                            tmp [1]='a';
+
+                        }
+            
+        }
+        public void leftRight() {
+                synchronized (left) {
+                    synchronized (right) {
+                       doSomething (number); 
+                        
+                    }
+                }
+       }
+
+       public void rightLeft() {
+            synchronized (right) {
+                synchronized (left) {
+                    doSomething(number); 
+                }
+            }
+        }
+       
+       public void run() {
+		try {
+			while (true) {
+				leftRight();
+                                rightLeft();
+			}
+		} catch (Exception e) {
+	              System.out.println ( e.getMessage());
+                      System.out.flush();
+		}
+	}
+
+    }
+    
+    public static void main ( String[] args ){
+        LeftRightDeadLock first = new LeftRightDeadLock( );
+        LeftRightDeadLock second = new LeftRightDeadLock( );
+        LeftRightDeadLock third = new LeftRightDeadLock( );
+       
+        new Thread( first).start();
+        new Thread( second).start();
+        new Thread( third).start();
+           
+           
+  
+    
+    }
+    
+}
