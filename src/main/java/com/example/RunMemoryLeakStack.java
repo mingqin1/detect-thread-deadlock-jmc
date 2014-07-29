f03659d1ae5e0e338c762c0750cda9376c0a1c5e
ERIN <ERIN@MARS>
2014-07-28 22:23:47 -0700

    Merge origin/master
    Conflicts:
    	src/main/java/com/example/RunMemoryLeakStack.java

--- src/main/java/com/example/RunMemoryLeakStack.java
+++ src/main/java/com/example/RunMemoryLeakStack.java
@@ -14,7 +14,7 @@ import java.util.EmptyStackException;
  */
 public class RunMemoryLeakStack {
 
-    boolean a = true;
+    int a=2;
     private static class Stack {
 
         private Object[] elements;
